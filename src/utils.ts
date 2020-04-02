48a52007901611408685238d0ef42eafa9d7e5c0
zou wendi <542095059@qq.com>
2020-04-02 16:25:14 +0800

    增加 scrollToField 方法

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+/**
+ * 常用的moment日期格式化枚举
+ */
 export enum DateFormat {
   sec = 'YYYY-MM-DD HH:mm:ss',
   min = 'YYYY-MM-DD HH:mm',
@@ -6,3 +9,30 @@ export enum DateFormat {
   month = 'YYYY-MM',
   year = 'YYYY',
 }
+
+/**
+ * 跳转到表单字段处,如果可以focus则自动focus
+ * @param fieldKey 字典id
+ */
+export const scrollToField = (fieldKey?: string) => {
+  try {
+    const inputNode = document.querySelector(
+      `#${fieldKey?.replace(/\./g, '-')}`,
+    );
+    if (inputNode) {
+      (inputNode as Element & { focus?: () => {} }).focus?.();
+      inputNode.scrollIntoView();
+    } else {
+      const labelNode = document.querySelector(
+        `label[for="${fieldKey?.replace(/\./g, '-')}"]`,
+      );
+      if (labelNode) {
+        labelNode.scrollIntoView({
+          block: 'center',
+        });
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
