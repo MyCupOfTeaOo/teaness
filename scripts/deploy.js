c776961758e53cc04d908f0b1b3a0cf7c109ff58
zou wendi <542095059@qq.com>
2020-04-24 13:40:00 +0800

    修改成更漂亮的打印

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,13 @@
 const { execSync } = require('child_process');
 const inquirer = require('inquirer');
-
 const fs = require('fs');
+const signale = require("signale");
+signale.config({
+  displayFilename: true,
+  displayTimestamp: true
+});
+
+
 
 inquirer
   .prompt({
@@ -11,7 +17,7 @@ inquirer
     default: '版本更新', // 默认值
   })
   .then(answer => {
-    console.log('准备构建组件');
+    signale.pending('准备构建组件');
     execSync('father-build', { stdio: 'inherit' });
 
     const package = JSON.parse(fs.readFileSync('./package.json'));
@@ -23,17 +29,17 @@ inquirer
       encoding: 'utf8',
     });
 
-    console.log('clean tgz');
     execSync('rimraf ./*tgz', { stdio: 'inherit' });
+    signale.success('clean tgz');
 
-    console.log('发布');
+    signale.pending('发布');
     execSync('npm pack', { stdio: 'inherit' });
     execSync('npm publish', { stdio: 'inherit' });
 
-    console.log('提交到版本库');
+    signale.pending('提交到版本库');
     execSync('git add -A', { stdio: 'inherit' });
     execSync(`git commit -m"${answer.commit}"`, { stdio: 'inherit' });
     execSync('git push', { stdio: 'inherit' });
 
-    console.log('finish');
+    signale.success('发布成功');
   });
