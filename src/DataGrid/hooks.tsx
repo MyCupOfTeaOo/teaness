5d2761684a6314599129e4358797fa5a41626221
zou wendi <542095059@qq.com>
2020-05-12 11:29:01 +0800

    fix useDataGrid location 丢失bug

--- src/DataGrid/hooks.tsx
+++ src/DataGrid/hooks.tsx
@@ -33,6 +33,7 @@ export function useDataGrid<T extends { [key: string]: any }>(
     gridProps: {
       ref: gridRef,
       historyId,
+      location,
       queryData: queryDataRef.current,
     },
     gridRef,
