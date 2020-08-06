ef01bd9902ab5d105f2b770c0f2e594a7b25add1
zou wendi <542095059@qq.com>
2020-08-06 16:19:20 +0800

    feat(FooterToolbar): 修改成functional组件增加参数resize

--- src/FooterToolbar/index.tsx
+++ src/FooterToolbar/index.tsx
@@ -1,57 +1,55 @@
-import React, { Component } from 'react';
+import React, { useEffect, useContext } from 'react';
 
 import classNames from 'classnames';
 import './index.scss';
+import { useValue } from '../hooks';
 
 export interface FooterToolbarProps {
   style?: React.CSSProperties;
   className?: string;
-  /**
-   * 手机端
-   */
-  isMobile?: boolean;
+  resize?: boolean;
 }
 
-export default class FooterToolbar extends Component<FooterToolbarProps> {
-  state = {
-    width: undefined,
+export const FooterToolbarContext = React.createContext<FooterToolbarProps>({});
+
+const FooterToolbar: React.FC<FooterToolbarProps> = props => {
+  const width = useValue<string | number>();
+  const context = useContext(FooterToolbarContext);
+  const { children, className, style, resize = true } = {
+    ...context,
+    ...props,
   };
-
-  componentDidMount() {
-    window.addEventListener('resize', this.resizeFooterToolbar);
-    this.resizeFooterToolbar();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.resizeFooterToolbar);
-  }
-
-  resizeFooterToolbar = () => {
-    const sider = document.querySelector('.ant-layout-sider') as HTMLDivElement;
-    if (sider == null) {
+  useEffect(() => {
+    if (!resize) {
       return;
     }
-    const { isMobile } = this.props;
-    const width = isMobile ? null : `calc(100% - ${sider.style.width})`;
-    const { width: stateWidth } = this.state;
-    if (stateWidth !== width) {
-      this.setState({ width });
-    }
-  };
-
-  render() {
-    const { children, className, style, ...restProps } = this.props;
-    const { width } = this.state;
-    return (
-      <div className="tea-footer-toolbar-layout">
-        <div
-          className={classNames(className, 'tea-footer-toolbar')}
-          style={{ width, ...style }}
-          {...restProps}
-        >
-          {children}
-        </div>
+    const resizeFooterToolbar = () => {
+      const sider = document.querySelector(
+        '.ant-layout-sider',
+      ) as HTMLDivElement;
+      if (sider == null) {
+        width.setValue(undefined);
+        return;
+      }
+      const newWidth = `calc(100% - ${sider.style.width})`;
+      width.setValue(newWidth);
+    };
+    resizeFooterToolbar();
+    window.addEventListener('resize', resizeFooterToolbar);
+    return () => {
+      window.removeEventListener('resize', resizeFooterToolbar);
+    };
+  }, [resize]);
+  return (
+    <div className="tea-footer-toolbar-layout">
+      <div
+        className={classNames(className, 'tea-footer-toolbar')}
+        style={{ width: width.value, ...style }}
+      >
+        {children}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default FooterToolbar;
